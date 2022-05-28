96a92c99419a2af4579e2d885a1a8fb6abb5c690
Santiago Barajas <santiago.barajas@berkeley.edu>
2022-05-28 06:48:25 -0700

    Implemented GET request that returns all the crops in CropController

--- src/main/java/com/example/helloworld/CropController.java
+++ src/main/java/com/example/helloworld/CropController.java
@@ -1,9 +1,6 @@
 package com.example.helloworld;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,13 +11,15 @@ public class CropController {
     public CropController(List<Crop> cropList) {
         this._cropList = cropList;
     }
-
     @RequestMapping(method= RequestMethod.POST, value = "/crops")
     public String addCrop(@RequestBody Crop crop) {
         _cropList.add(crop);
         return crop.getName() + " added";
     }
-
+    @GetMapping("/crops")
+    public String all() {
+        return toString();
+    }
     @Override
     public String toString() {
         return _cropList.stream()

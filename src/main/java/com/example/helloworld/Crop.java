33411f43d260fc3e5aedc9eb6cc29b0dd1de2ac2
Santiago Barajas <santiago.barajas@berkeley.edu>
2022-05-27 18:01:59 -0700

    Got first POST request working

--- src/main/java/com/example/helloworld/Crop.java
+++ src/main/java/com/example/helloworld/Crop.java
@@ -1,5 +1,21 @@
 package com.example.helloworld;
 
-public abstract class Crop {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Crop {
+   @JsonCreator
+    public Crop(@JsonProperty("_name") String name) {
+        this._name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String _name) {
+        this._name = _name;
+    }
+
+    private String _name;
 }
--- src/main/java/com/example/helloworld/CropController.java
+++ src/main/java/com/example/helloworld/CropController.java
@@ -1,15 +1,24 @@
 package com.example.helloworld;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CropController {
 
-    @RequestMapping(method=RequestMethod.POST, value = "/crops")
-    public void addCrop(@Re) {
-
+    public CropController(List<Crop> cropList) {
+        this._cropList = cropList;
     }
 
+    @RequestMapping(method= RequestMethod.POST, value = "/crops")
+    public List<Crop> addCrop(@RequestBody Crop crop) {
+        _cropList.add(crop);
+        return _cropList;
+    }
+
+    private List<Crop> _cropList;
 }

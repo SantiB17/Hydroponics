cf02804e2fef8ccdc91e1762dfc34e66301c5548
Santiago Barajas <santiago.barajas@berkeley.edu>
2022-05-28 07:16:31 -0700

    Setting up Crop class for persistence

--- src/main/java/com/example/helloworld/Crop.java
+++ src/main/java/com/example/helloworld/Crop.java
@@ -3,21 +3,42 @@ package com.example.helloworld;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Crop {
+
+    public Crop() {
+    }
    @JsonCreator
     public Crop(@JsonProperty("name") String name, @JsonProperty("dateHarvested") String dateHarvested) {
         this._name = name;
         this._datePlanted = dateHarvested;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return _name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String _name) {
         this._name = _name;
     }
 
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private String _name;
     private String _datePlanted;
+
 }
